b6029316793ba7a0daa25e7f7250545a7559a2ea
Jin Kim <jinwoov@gmail.com>
2020-12-22 16:32:21 -0600

    jk

--- github_automation/main.py
+++ github_automation/main.py
@@ -1,10 +1,10 @@
-import os
+import os, subprocess
 
 
 os.system("git add .")
 user_commit_message = input("Commit message.. ")
 os.system(f"git commit -m {user_commit_message}")
 user_branch = input("Brnach? ")
-os.system(f"git branch -r --contains {user_branch}")
-
-os.system(f"git push origin {user_branch}")
+output = subprocess.Popen(["git", "branch", "-r", "--contains", user_branch])
+print(output)
+# os.system(f"git push origin {user_branch}")
