7c6a6f613c84c16dc82ecbf69b6d4c2b409ecf1a
Jin Kim <jinwoov@gmail.com>
2020-12-22 16:20:26 -0600

    Testing

--- github_automation/main.py
+++ github_automation/main.py
@@ -4,5 +4,12 @@ import os
 os.system("git add .")
 user_commit_message = input("Commit message.. ")
 os.system(f"git commit -m {user_commit_message}")
-user_branch = input("Brnach? ")
-os.system(f"git push origin {user_branch}")
+was_not_push = True
+while was_not_push:
+    try:
+        user_branch = input("Brnach? ")
+        os.system(f"git push origin {user_branch}")
+        was_not_push = False
+    except Exception:
+        print("Please enter valid branch.. ")
+        continue
