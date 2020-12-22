27e55d119569e9039cb39a810a2a8e21d6cb7d28
Jin Kim <jinwoov@gmail.com>
2020-12-22 16:37:20 -0600

    hello

--- github_automation/main.py
+++ github_automation/main.py
@@ -4,7 +4,11 @@ import os, subprocess
 os.system("git add .")
 user_commit_message = input("Commit message.. ")
 os.system(f"git commit -m {user_commit_message}")
-user_branch = input("Brnach? ")
-output = subprocess.Popen(["git", "branch", "-r", "--contains", user_branch])
-print(output)
+user_branch = input("Branch? ")
+cmd = f"git branch -r --contains {user_branch}"
+output = os.Popen(cmd)
+while output.__contains__("error"):
+    user_branch = input("What branch do you want to push to? ")
+    cmd = f"git branch -r --contains {user_branch}"
+    output = os.Popen(cmd)
 # os.system(f"git push origin {user_branch}")
