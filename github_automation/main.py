2ae70ddda16dd623e742f2f8733d6195263c6a28
Jin Kim <jinwoov@gmail.com>
2020-12-22 17:02:00 -0600

    done with everything

--- github_automation/main.py
+++ github_automation/main.py
@@ -4,11 +4,13 @@ import os, subprocess
 os.system("git add .")
 ## Commit message and committing
 user_commit_message = input("Commit message.. ")
-os.system(f"git commit -m '{user_commit_message}'")
-
+os.system(f"git commit -m '{str(user_commit_message)}'")
+print()
 # Listing the branches
+print("#==========List of branches==========")
 os.system("git branch")
-
+print("#====================================")
+print()
 ## Asking for which branch and pushing
 user_branch = input("Branch? ")
 os.system(f"git push origin {user_branch}")
