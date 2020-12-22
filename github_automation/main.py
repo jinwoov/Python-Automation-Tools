464cc14aaf8383f9da4ba337954edcd04101c7d5
Jin Kim <jinwoov@gmail.com>
2020-12-22 16:37:40 -0600

    fixed

--- github_automation/main.py
+++ github_automation/main.py
@@ -6,9 +6,9 @@ user_commit_message = input("Commit message.. ")
 os.system(f"git commit -m {user_commit_message}")
 user_branch = input("Branch? ")
 cmd = f"git branch -r --contains {user_branch}"
-output = os.Popen(cmd)
+output = os.popen(cmd)
 while output.__contains__("error"):
     user_branch = input("What branch do you want to push to? ")
     cmd = f"git branch -r --contains {user_branch}"
-    output = os.Popen(cmd)
+    output = os.popen(cmd)
 # os.system(f"git push origin {user_branch}")
