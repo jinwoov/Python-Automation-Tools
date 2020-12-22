f79b172ae447ce339e37628636710d2edd2e0fed
Jin Kim <jinwoov@gmail.com>
2020-12-22 16:41:53 -0600

    'master'

--- github_automation/main.py
+++ github_automation/main.py
@@ -1,8 +1,14 @@
 import os, subprocess
 
-
+## Git adding
 os.system("git add .")
+## Commit message and committing
 user_commit_message = input("Commit message.. ")
 os.system(f"git commit -m '{user_commit_message}'")
+
+# Listing the branches
+os.system("git branch")
+
+## Asking for which branch and pushing
 user_branch = input("Branch? ")
 os.system(f"git push origin {user_branch}")
